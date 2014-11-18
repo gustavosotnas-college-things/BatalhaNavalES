2b5c829d4da1e92ad3e03a951678d5017ae84035
gustavosotnas <gustavo_moraiss@hotmail.com>
2014-11-18 19:32:17 -0200

    Alterado o nome do pacote do projeto.

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -7,8 +7,8 @@ package battleship;
 
 public class Main
 {
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
         System.out.println("Hello.GORDO");
     }
     
