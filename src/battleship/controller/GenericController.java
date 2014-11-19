d69ba6ef685df96b46e16233f3895e0490a2a075
gustavosotnas <gustavo_moraiss@hotmail.com>
2014-11-19 15:23:10 -0200

    Adicionado comentário sobre a classe GenericController

--- src/battleship/controller/GenericController.java
+++ src/battleship/controller/GenericController.java
@@ -1,3 +1,6 @@
+/* Classe genérica para colocar funções relacionadas ao controle do jogo, 
+pode alterar o nome ou colocar mais classes dentro desse pacote depois */
+
 package battleship.controller;
 
 public class GenericController {
