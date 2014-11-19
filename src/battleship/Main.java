61624e1b65d90b36052ba18bd0b348f8711179d2
RenanOfugi <renangyn2010@hotmail.com>
2014-11-19 01:34:12 -0200

    fatoração do codigo (teste)

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -1,9 +1,7 @@
 package battleship;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        
+public class Main {
+
+    public static void main(String[] args) {
     }
 }
