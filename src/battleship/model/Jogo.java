acea87169ccbc4544bf4e78bd38d4cb331fd7c89
gustavosotnas <gustavo_moraiss@hotmail.com>
2014-11-20 02:59:20 -0200

    Criada a classe "Jogo", que conterá as informações a respeito de tudo o que foi escolhido pelo usuário.

---
+++ src/battleship/model/Jogo.java
@@ -0,0 +1,34 @@
+package battleship.model;
+
+public class Jogo
+{
+    private int modoDeJogo; //1 para "Modo Tradicional" e 2 para "Modo PQQD"
+    private int dificuldadeDoJogo; //1 para "Fácil (9x9)", 2 para "Intermediário (12x12)" e 3 para "Difícil (15x15)"
+    private int modoDeDistribuicao; //1 para Distribuição da frota "Automática" e 2 para Distribuição da frota "Manual"
+    
+    //OBS.: depois temos que aplicar o Strategy para um desses campos, igual o problema do pato.
+
+    public int getModoDeJogo() {
+        return modoDeJogo;
+    }
+
+    public void setModoDeJogo(int modoDeJogo) {
+        this.modoDeJogo = modoDeJogo;
+    }
+
+    public int getDificuldadeDoJogo() {
+        return dificuldadeDoJogo;
+    }
+
+    public void setDificuldadeDoJogo(int dificuldadeDoJogo) {
+        this.dificuldadeDoJogo = dificuldadeDoJogo;
+    }
+
+    public int getModoDeDistribuicao() {
+        return modoDeDistribuicao;
+    }
+
+    public void setModoDeDistribuicao(int modoDeDistribuicao) {
+        this.modoDeDistribuicao = modoDeDistribuicao;
+    }
+}
