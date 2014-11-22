05bb2bd4d1cb123038a9b3d4f580ac5567356ba8
RenanOfugi <renangyn2010@hotmail.com>
2014-11-22 21:16:14 -0200

    Fatorado

--- src/battleship/controller/BattleshipGameException.java
+++ src/battleship/controller/BattleshipGameException.java
@@ -1,14 +1,13 @@
 package battleship.controller;
 
-public class BattleshipGameException extends BattleshipException
-{
-    public BattleshipGameException (String mensagem)
-    {
+public class BattleshipGameException extends BattleshipException {
+
+    public BattleshipGameException(String mensagem) {
         super(mensagem);
     }
+
     //sobrecarga (mensagem default)
-    public BattleshipGameException ()
-    {
+    public BattleshipGameException() {
         super("EXCEÇÃO: Algum problema aconteceu durante a execução do jogo.");
     }
 }
