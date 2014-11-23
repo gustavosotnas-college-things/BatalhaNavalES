e0123304d65a0757973b6f1f9190b7a17fe150bc
gustavosotnas <gustavosotnas1@gmail.com>
2014-11-23 11:07:07 -0200

    Criado o construtor da classe Jogo.

--- src/battleship/model/Jogo.java
+++ src/battleship/model/Jogo.java
@@ -4,35 +4,14 @@ import battleship.model.atributosDeJogo.*;
 
 public class Jogo
 {
-    //private int modoDeJogo; //1 para "Modo Tradicional" e 2 para "Modo PQQD"
     private ModoDeJogo modoDeJogo;
-    //private int dificuldadeDoJogo; //1 para "Fácil (9x9)", 2 para "Intermediário (12x12)" e 3 para "Difícil (15x15)"
     private DificuldadeDoJogo dificuldadeDoJogo;
-    private int modoDeDistribuicao; //1 para Distribuição da frota "Automática" e 2 para Distribuição da frota "Manual"
-    
-    //OBS.: depois temos que terminar de aplicar o Strategy para um desses campos, igual o problema do pato.
-    
-    /*    public int getModoDeJogo() {
-        return modoDeJogo;
-    }
+    private ModoDeDistribuicao modoDeDistribuicao;
 
-    public void setModoDeJogo(int modoDeJogo) {
+    //Construtor
+    public Jogo(ModoDeJogo modoDeJogo, DificuldadeDoJogo dificuldadeDoJogo, ModoDeDistribuicao modoDeDistribuicao) {
         this.modoDeJogo = modoDeJogo;
-    }
-
-    public int getDificuldadeDoJogo() {
-        return dificuldadeDoJogo;
-    }
-
-    public void setDificuldadeDoJogo(int dificuldadeDoJogo) {
         this.dificuldadeDoJogo = dificuldadeDoJogo;
-    }
-
-    public int getModoDeDistribuicao() {
-        return modoDeDistribuicao;
-    }
-*/
-    public void setModoDeDistribuicao(int modoDeDistribuicao) {
         this.modoDeDistribuicao = modoDeDistribuicao;
     }
 }
