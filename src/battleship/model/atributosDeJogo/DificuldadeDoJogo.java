43251187945b3f6dc2812f14055affb93cbb333a
RenanOfugi <renangyn2010@hotmail.com>
2014-11-23 11:35:33 -0200

    modificado o tipo de coleção para matriz

--- src/battleship/model/atributosDeJogo/DificuldadeDoJogo.java
+++ src/battleship/model/atributosDeJogo/DificuldadeDoJogo.java
@@ -4,18 +4,18 @@ import battleship.model.elementos.Celula;
 import java.util.ArrayList;
 
 public abstract class DificuldadeDoJogo {
-    
-        private int tamanhoTabuleiro; //variável abstrata
-        
-        public Object InicializaTabuleiro() {
+
+    private int tamanhoTabuleiro; //variável abstrata
+
+    public Object InicializaTabuleiro() {
         Celula[][] listaTabuleiro = new Celula[tamanhoTabuleiro][tamanhoTabuleiro];
 
         for (int i = 0; i < tamanhoTabuleiro; i++) {
-            for(int j = 0; j < tamanhoTabuleiro; j++){
+            for (int j = 0; j < tamanhoTabuleiro; j++) {
                 Celula celula = new Celula();
                 listaTabuleiro[i][j] = celula;
             }
-            
+
         }
         return listaTabuleiro;
     }
