d2fd581cd87c120df3e8bd186ca0530f9b23555f
RenanOfugi <renangyn2010@hotmail.com>
2014-11-30 23:23:08 -0200

    Documentação das classes e funções (javaDoc)

--- src/battleship/model/elementos/DecoradorElemento.java
+++ src/battleship/model/elementos/DecoradorElemento.java
@@ -1,5 +1,10 @@
 package battleship.model.elementos;
 
+/**
+ * Classe que permite a decoração de um objeto base em tempo de execução.
+ * @author Renan O
+ * @author Gustavo Moraes
+ */
 public abstract class DecoradorElemento extends Celula {
     public abstract String whoami();
     public abstract String getNome();
